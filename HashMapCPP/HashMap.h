35947175871c79bea39fa96cb6625cf015351b75
Dylan.Ochoa <dryan8a@gmail.com>
2020-11-06 17:28:59 -0800

    Finished

--- HashMapCPP/HashMap.h
+++ HashMapCPP/HashMap.h
@@ -15,8 +15,8 @@ public:
 	static std::unique_ptr<HashMap<TKey,TValue>> Create(int startingLength, double maxLoadFactor = 0.75);
 	bool Add(TKey key,TValue value);
 	bool Remove(TKey key);
-	bool Contains(TKey key);
-	TValue Find(TKey key);
+	bool Contains(TKey key) const;
+	std::unique_ptr<TValue> Find(TKey key) const;
 	int Count;
 };
 
@@ -48,8 +48,8 @@ bool HashMap<TKey, TValue>::Add(TKey key, TValue value)
 	{
 		if (Data[bucketIndex][i].first == key) return false;
 	}
-	//Data[bucketIndex].emplace_back(std::make_pair<TKey, TValue>(key, value));
-	Data[bucketIndex].emplace_back({ key, value });
+	//Data[bucketIndex].push_back(std::make_pair<TKey, TValue>(key, value));
+	Data[bucketIndex].emplace_back(key, value);
 	Keys.push_back(key);
 	Count++;
 	if ((double)Count/Data.size() > MaxLoadFactor) ReHash();
@@ -59,13 +59,71 @@ bool HashMap<TKey, TValue>::Add(TKey key, TValue value)
 template<typename TKey, typename TValue>
 void HashMap<TKey,TValue>::ReHash()
 {
-	auto newMap = std::vector<std::vector<std::pair<TKey, TValue>>>{ Data.size()*2 };
+	std::vector<std::vector<std::pair<TKey,TValue>>> newMap = std::vector<std::vector<std::pair<TKey, TValue>>>{ Data.size()*2 };
 	for (int i = 0; i < Keys.size(); i++)
 	{
-		// get the new bucket index
-		// go to that bucket index and push_back
-		newMap[Hash(Keys[i]) % newMap.size()].push_back({ Keys[i],Data[Hash(Keys[i]) % Data.size()][Keys[i]]});
+		int newBucketIndex = Hash(Keys[i]) % newMap.size();
+		int oldBucketIndex = Hash(Keys[i]) % Data.size();
+		for (auto pair : Data[oldBucketIndex])
+		{
+			if (pair.first == Keys[i])
+			{
+				newMap[newBucketIndex].emplace_back(Keys[i], pair.second);
+				break;
+			}
+		}
 	}
 	Data = std::move(newMap);
 }
 
+template<typename TKey,typename TValue>
+bool HashMap<TKey, TValue>::Remove(TKey key)
+{
+	int bucketIndex = Hash(key) % Data.size();
+	bool didFind = false;
+	for (int i = 0; i < Data[bucketIndex].size(); i++)
+	{
+		if (Data[bucketIndex][i].first == key)
+		{
+			Data[bucketIndex].erase(std::find(Data[bucketIndex].begin(),Data[bucketIndex].end(),Data[bucketIndex][i]));
+			didFind = true;;
+		}
+	}
+	if (!didFind) return false;
+	Keys.erase(std::find(Keys.begin(), Keys.end(), key));
+	Count--;
+	return true;
+}
+
+template<typename TKey,typename TValue>
+bool HashMap<TKey, TValue>::Contains(TKey key) const
+{
+	auto iterator = std::find(Keys.begin(), Keys.end(), key);
+	if (iterator == Keys.end()) return false;
+	return true;
+}
+
+template<typename TKey, typename TValue>
+std::unique_ptr<TValue> HashMap<TKey, TValue>::Find(TKey key) const
+{
+	int bucketIndex = Hash(key) % Data.size();
+	for (auto pair : Data[bucketIndex])
+	{
+		if (pair.first == key) return std::make_unique<TValue>(pair.second);
+	}
+	return nullptr;
+}
+
+//template <typename T>
+//int IndexOf(std::vector<T>& vec, T& item)
+//{
+//	auto itr = std::find(vec.begin(), vec.end(), item);
+//
+//	if (itr != vec.end())
+//	{
+//		// vec.erase(itr);
+//		return std::distance(vec.begin(), itr);
+//	}
+//
+//	return -1;
+//}
--- HashMapCPP/main.cpp
+++ HashMapCPP/main.cpp
@@ -3,7 +3,6 @@
 
 int main()
 {
-    //Visual Studio bad, fix weird rehash add problem thing next time I guess sure, why not
     HashMap<std::string, int> map{5,0.75};
     map.Add("Richard", 69);
     map.Add("Snoop Dogg", 420);
@@ -12,6 +11,8 @@ int main()
     map.Add("Ho Lee Fuk", 42);
     map.Add("Sum Ting Wong", 42);
     map.Add("Wi Tu Lo", 42);
-
+    map.Remove("Richard");
+    std::cout << map.Contains("Richard") << map.Contains("Mike Hunt") << std::endl;
+    auto SnoopDogg = map.Find("Snoop Dogg");
     return 0;
 }
